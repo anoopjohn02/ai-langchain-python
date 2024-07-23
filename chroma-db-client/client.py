69b08956e1d4fbdbf0d712f19e7d619b5cac9315
Anoop John <anoopjohn02@gmail.com>
2024-07-23 19:31:46 +0530

    chroma db client

--- chroma-db-client/client.py
+++ chroma-db-client/client.py
@@ -9,13 +9,13 @@ chroma_client = chromadb.HttpClient(host='localhost', port=8000)
 
 default_ef = embedding_functions.OpenAIEmbeddingFunction(api_key=os.getenv("OPENAI_API_KEY"))
 
-collection = chroma_client.get_collection(name="my-collection", embedding_function=default_ef)
+collection = chroma_client.get_collection(name="spring-collection", embedding_function=default_ef)
 
 results = collection.query(
     query_texts=["What is spice"],
     n_results=2,
     include=["metadatas"],
-    where={"userId": "teena"}, # optional filter
+    where={"userId": "anoop"}, # optional filter
     # where_document={"$contains":"search_string"}  # optional filter
 )
 
